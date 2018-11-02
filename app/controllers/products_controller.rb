71611444c5a2ca94789ee255aacf24d98efc8a48
dschadd <dschadd@gmail.com>
2018-11-01 19:47:23 -0500

    add product create html

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -3,11 +3,35 @@ class ProductsController < ApplicationController
   def index
     @products = Product.all
     render "index.html.erb"
+
+  end
+
+  def new
+    render "new.html.erb"
+  end
+
+  def create
+
+    @product = Product.new(
+      name: params[:name],
+      price: params[:price],
+      description: params[:description],
+      supplier_id: params[:supplier_id],
+      image_url: params[:image_url]
+      )
+    @product.save
+    redirect_to "/products/#{@product.id}"
+
   end
 
   def show
     @product = Product.find_by(id: params[:id])
     render "show.html.erb"
+  end
+
+  def edit
+    @product = Product.find_by(id: params[:id])
+    render "edit.html.erb"
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -37,6 +37,9 @@ Rails.application.routes.draw do
   end
   
   get "/products" => "products#index"
+  get "/products/new" => "products#new"
+  post "/products" => "products#create"
   get "/products/:id" => "products#show"
+  get "/products/:id/edit" => "products#edit"
 
 end
