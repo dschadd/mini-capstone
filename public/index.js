973b70f54643a3ed9e0e16b58096babfc124f6b7
dschadd <dschadd@gmail.com>
2018-11-13 18:24:35 -0600

    remove image_url from product model

--- public/index.js
+++ public/index.js
@@ -1,6 +1,20 @@
-var rocketTemplate = document.querySelector("#rocket-card");
-var rocketContainer = document.querySelector(".row");
+/* global axios */
 
-rocketContainer.appendChild(rocketTemplate.content.cloneNode(true));
-rocketContainer.appendChild(rocketTemplate.content.cloneNode(true));
-rocketContainer.appendChild(rocketTemplate.content.cloneNode(true));
+axios.get("/api/products").then(function(response) {
+  var products = response.data;
+  console.log("the products are", products);
+
+  var rocketTemplate = document.querySelector("#rocket-card");
+  var rocketContainer = document.querySelector(".row");
+
+  products.forEach(function(product) {
+    var copiedContent = rocketTemplate.content.cloneNode(true);
+    if (product.images[0]) {
+      copiedContent.querySelector(".card-img-top").src =
+        product.images[0].image_url;
+    }
+    copiedContent.querySelector(".card-title").innerText = product.name;
+    copiedContent.querySelector(".card-text").innerText = product.description;
+    rocketContainer.appendChild(copiedContent);
+  });
+});
